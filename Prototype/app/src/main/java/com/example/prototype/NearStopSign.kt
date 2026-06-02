70ad97af443f9963c84b699b2534e561297cf83b
kuwahara-gennki <262702900+kuwahara-gennki@users.noreply.github.com>
2026-06-02 10:11:11 +0900

    メインの処理が出来ました。

--- Prototype/app/src/main/java/com/example/prototype/NearStopSign.kt
+++ Prototype/app/src/main/java/com/example/prototype/NearStopSign.kt
@@ -1,6 +1,9 @@
 package com.example.prototype
 import android.location.Location
 import com.example.prototype.LocationGetter
+
+//data class
+
 class NearStopSign() {
     private lateinit var location: Location
     val stopPoints = mutableListOf<Map<String, Any>>()
@@ -27,7 +30,7 @@ class NearStopSign() {
             )
         )
     }
-    fun matchBearing(
+    private fun matchBearing(
         userBearing: Float,
         stopPoints: List<Map<String, Any>>
     ): List<Map<String, Any>> {
@@ -40,23 +43,30 @@ class NearStopSign() {
         }
 
     }
-    fun getBearingDiff(a: Float,b: Float): Float{
+    private fun getBearingDiff(a: Float,b: Float): Float{
         val diff = kotlin.math.abs(a-b)
         return kotlin.math.min(
             diff,
             360f-diff
         )
     }
-    fun matchNearestStop(nowLat: Double,nowLong: Double, matchBearingPoints:List<Map<String, Any>>):Map<String, Any>{
+    private fun matchNearestStop(nowLat: Double,nowLong: Double, matchBearingPoints:List<Map<String, Any>>):Map<String, Any>?{
         var results = FloatArray(3)
         val distances = mutableListOf<Float>()
+        if (matchBearingPoints.isEmpty()) return null
+
+
         for (i in 0..< matchBearingPoints.size){
             Location.distanceBetween(nowLat,nowLong,matchBearingPoints[i]["lat"] as Double,matchBearingPoints[i]["long"] as Double, results)
             distances.add(results[0])
         }
         return matchBearingPoints[distances.indexOf(distances.minOrNull())]
     }
-    fun matchStopSing(lat: Double,long: Double,bearing: Float,stopPoints: List<Map<String, Any>>):Map<String, Any>{
-        return matchNearestStop(lat,long,matchBearing(bearing,stopPoints))
+    //最寄りの一時停止返す関数
+    fun matchStopSing(lat: Double,long: Double,bearing: Float,stopPoints: List<Map<String, Any>>):Map<String, Any>?{
+        var matchBearingPoints = matchBearing(bearing,stopPoints)
+        if (matchBearingPoints.isEmpty()) return null
+            return matchNearestStop(lat, long, matchBearingPoints)
+
     }
 }
