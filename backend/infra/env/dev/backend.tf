9e11644b96596e8c041b267042614278801b7676
暁月蒼空 <80885196+AkatukiSora@users.noreply.github.com>
2026-06-18 11:32:33 +0900

    Enable S3 state locking for dev backend

--- backend/infra/env/dev/backend.tf
+++ backend/infra/env/dev/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "gr9-terraform-state-afbf602e"
-    key    = "env/dev/terraform.tfstate"
-    region = "ap-northeast-3"
+    bucket="gr9-terraform-state-afbf602e"
+    key="env/dev/terraform.tfstate"
+    region="ap-northeast-3"
+    use_lockfile=true
   }
 }
