f874a4dea2778431492d531c4797f97892948865
いやくん <iyada0311@gmail.com>
2026-07-13 10:19:40 +0900

    Update FirstLaunchCheck.kt

--- Prototype/app/src/main/java/com/example/prototype/FirstLaunchCheck.kt
+++ Prototype/app/src/main/java/com/example/prototype/FirstLaunchCheck.kt
@@ -3,48 +3,52 @@ package com.example.prototype
 import android.content.Context
 import androidx.core.content.edit
 
-class FirstLaunchCheck(private val context: Context) {
+class FirstLaunchCheck(context: Context) {
+
     private val prefs = context.getSharedPreferences(
         "modeSettings",
         Context.MODE_PRIVATE
     )
-    //初回起動かどうか
+
+    // 初回起動かどうか
     fun isFirstLaunch(): Boolean {
-        return  prefs.getBoolean(
+        return prefs.getBoolean(
             KEY_FIRST_LAUNCH,
             true
         )
     }
 
-    //初回起動が終わったことを記録する
+    // 初回起動が終わったことを記録する
     fun setFirstLaunchFinished() {
-        prefs.edit()
-            .putBoolean(
+        prefs.edit {
+            putBoolean(
                 KEY_FIRST_LAUNCH,
                 false
             )
         }
     }
 
-    // 追加：初回起動状態に戻す
+    // 初回起動状態に戻す
     fun resetFirstLaunch() {
-        prefs.edit()
-            .putBoolean(
+        prefs.edit {
+            putBoolean(
                 KEY_FIRST_LAUNCH,
                 true
             )
         }
     }
 
+    // 通知モードを保存する
     fun saveAlertMode(mode: String) {
-        prefs.edit()
-            .putString(
+        prefs.edit {
+            putString(
                 KEY_ALERT_MODE,
                 mode
             )
-            .apply()
+        }
     }
 
+    // 保存されている通知モードを取得する
     fun getAlertMode(): String {
         return prefs.getString(
             KEY_ALERT_MODE,
